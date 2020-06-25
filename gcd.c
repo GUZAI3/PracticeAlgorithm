7dd23a58c0d92db45f695ac922f2fab89251e777
GUZAI3 <55838958+GUZAI3@users.noreply.github.com>
2020-06-25 22:34:25 +0900

    Add English comment

--- gcd.c
+++ gcd.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  unsigned int num1 = 1029;  // GCDを求めたいペアのひとつ
-  unsigned int num2 = 1071;  // GCDを求めたいペアのひとつ
+  unsigned int num1 = 1029;  // Natural number 1
+  unsigned int num2 = 1071;  // Natural number 2
 
-  // GCD計算
+  // Calculate GCD
   unsigned int a = num1;
   unsigned int b = num2;
-  unsigned int rem = a % b;  // 余り:Reminder
+  unsigned int rem = a % b;
   while (rem) {
     a = b;
     b = rem;
     rem = a % b;
   }
 
+  // Print out GCD
   printf("gcd(%u,%u) = %u\n", num1, num2, b);
   return 0;
 }
