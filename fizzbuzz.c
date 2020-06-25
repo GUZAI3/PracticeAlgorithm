bfecffd1ad6ec002999076c388246b52a387c789
GUZAI3 <55838958+GUZAI3@users.noreply.github.com>
2020-06-25 22:38:54 +0900

    Add English comment

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int end = 128;  // 出力数
+  int end = 128;  // Maximum value for which GUZAI3 want to calculate fizzbuzz.
 
   for (int n = 1; n < end; n++) {
     if (n % 15 == 0) {
